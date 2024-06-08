ceb1034112176d05b4f57dd59e02afedd639bf1a
VictorHugoVN <victorneivadef8@gmail.com>
2024-06-08 18:15:07 -0300

    Camada de Serviço implementada

---
+++ src/main/java/me/spring/service/UserService.java
@@ -0,0 +1,15 @@
+package me.spring.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+@Service
+public class UserService {
+
+	@Autowired
+	UserService userService;
+	
+	
+	
+}
